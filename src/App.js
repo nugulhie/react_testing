09bc1b19114812b5f5babd5a7ae0259c0de2335d
황혁주 <nugulhie@gmail.com>
2021-01-25 16:02:00 +0900

    edited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Temp({name}) {
+  return <h1>temp {name}</h1>
+}
+
+const tempIs = [
+  {
+    name : "1"
+  },
+  {
+    name : "2"
+  },
+  {
+    name : "3"
+  },
+  {
+    name : "4"
+  }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello
+    {tempIs.map(teem => <Temp name = {teem.name}/>)}
+    
     </div>
   );
 }
